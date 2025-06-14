560fa2505725da6cb02ba1a1fb60505f96c39e94
K Chetan <ck123456chetan@gmail.com>
2025-06-14 12:02:47 +0530

    Update main.go

--- question1/main.go
+++ question1/main.go
@@ -1,1 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := setupRouter()
+	log.Println("Server running on port 9876...")
+	log.Fatal(http.ListenAndServe(":9876", router))
+}
 
